9a3e07614335e59ed6fed809f206c8c86588788b
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-06-23 05:16:38 +0200

    AddedBtns&items

--- app.js
+++ app.js
@@ -17,9 +17,26 @@ form.addEventListener('submit', addItem);
 function addItem(e) {
   e.preventDefault();
   const value = grocery.value;
-  const id = new Date().getTime().toString();
-  if (value && editFlag) {
 
+  const id = new Date().getTime().toString();
+  if (value && !editFlag) {
+    const element = document.createElement('article');
+    element.classList.add('grocery-item');
+    const attr = document.createAttribute('data-id');
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = ` <p class="title">${value}</p>
+    <div class="btn-container">
+      <button type="button" class="edit-btn">
+        <i class="fas fa-edit"></i>
+      </button>
+      <button type="button" class="edit-btn" >
+        <i class="fas fa-trash"></i>
+      </button>
+    </div>`;
+    list.appendChild(element);
+    displayAlert("item added to the list", "success");
+    container.classList.add('show-container');
   } else if (value && editFlag) {
 
   } else { displayAlert("please enter value", "danger"); }
